ae219e2ed3b52140ae577ff8d4d6823df7fc5d56
Marcos Galvão <mgalvao@ciandt.com>
2022-06-27 14:25:00 -0300

    test of new print driver/version way

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,13 +1,17 @@
 import java.sql.*;
+import java.util.Enumeration;
 import java.util.stream.Collectors;
 
 public class PrintJDBCDrivers {
     public static void main(String[] args) {
-        DriverManager.drivers()
-                .forEach(d -> {
-                            System.out.println(d.getClass().getName());
-                            System.out.println(d.getMajorVersion());
-                        }
-                );
+//        DriverManager.drivers()
+//                .forEach(d -> {
+//                            System.out.println(d.getClass().getName());
+//                            System.out.println(d.getMajorVersion());
+//                        }
+//                );
+
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        System.out.println(drivers);
     }
 }
