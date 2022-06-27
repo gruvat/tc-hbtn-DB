05a7d38cf85c11bd70c3360ec059472474b72705
Marcos Galvão <mgalvao@ciandt.com>
2022-06-26 21:28:40 -0300

    task 3 SQLite

---
+++ 0x00/online/command.sql
@@ -0,0 +1,6 @@
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty7", "betty7@techcamps.com");
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty8", "betty8@techcamps.com");
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty9", "betty9@techcamps.com");
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty10", "betty10@techcamps.com");
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty11", "betty11@techcamps.com");
+INSERT INTO CLIENTE (nome, email) VALUES ("Betty12", "betty12@techcamps.com");
