6eca3590bf9421cb967379d24c433c0b488e05e1
Marcos Galvão <mgalvao@ciandt.com>
2022-06-28 18:05:42 -0300

    task 2 Data Manipulation

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE cliente (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,nome varchar(255),idade INTEGER,cpf varchar(255),rg varchar(255));
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
